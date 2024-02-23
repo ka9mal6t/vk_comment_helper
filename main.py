c7a567a145606558f25180b6f965222d782c720d
ka9mal6tik <vladimyr.kilko@gmail.com>
2024-02-23 17:26:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import vk_api
+import random
+from colorama import Fore
+from config import APP_ID
+
+from take_screenshots import take_screenshots
+
+FORCE_QUIT = False  # Quit if unknown error has occurred
+MESSAGES = []
+
+
+# Randomly change cyrillic characters to latin to avoid detection in VK
+def Obfuscate(msg):
+    if random.getrandbits(1):
+        msg = msg.replace('о', 'o')
+    if random.getrandbits(1):
+        msg = msg.replace('е', 'e')
+
+    return msg
+
+
+# Get list of messages to post on
+def GetAllMessage(file):
+    global MESSAGES
+    with open(file, 'r', encoding='utf-8') as file:
+        for line in file:
+            MESSAGES.append(line.strip())
+
+
+# Get random message
+def GetMessage():
+    global MESSAGES
+    return Obfuscate(random.choice(MESSAGES))
+
+
+# Get list of target links to post on
+def GetLinks(file):
+    with open(file) as links:
+        return links.read().split()
+
+
+# Convert link to post to ID
+def LinkToID(link):
+    return link.split("wall")[-1].split('_')
+
+
+# Captcha solver
+def SolveCaptcha(c):
+    print(Fore.RED + "[!] Captcha needed:", c.get_url())
+    key = input(Fore.GREEN + "[>] Please enter the code: ")
+    return c.try_again(key)
+
+
+def main():
+    global MESSAGES
+    global FORCE_QUIT
+    print(Fore.CYAN + "VK Commenter\n")
+
+    links = GetLinks("links.txt")
+    login = input("Login: ")
+    password = input("Password: ")
+    GetAllMessage("messages.txt")
+
+    vk_session = vk_api.VkApi(login=login, password=password, app_id=APP_ID, captcha_handler=SolveCaptcha)
+    vk_session.auth()
+
+    vk = vk_session.get_api()
+
+    print(Fore.GREEN + "[?] Login successful")
+
+    for link in links:
+        ids = LinkToID(link)
+        try:
+            vk.wall.createComment(owner_id=ids[0], post_id=ids[1], message=GetMessage())
+        except Exception as e:
+            if "parent deleted" in str(e).lower():
+                print(Fore.RED + f"[-] {e} ({link})")
+            else:
+                print(Fore.RED + f"[-] {e} ({link})")
+                if FORCE_QUIT:
+                    exit()
+        else:
+            print(Fore.GREEN + "[+] Posted on", link)
+            take_screenshots(link)
+            print(Fore.GREEN + "[+] Screenshot saved", link)
+
+
+if __name__ == "__main__":
+    main()
---
+++ take_screenshots.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import os
+
+
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def take_screenshots(url: str):
+    create_directory_if_not_exists("screenshots")
+    options = webdriver.ChromeOptions()
+
+    options.headless = True  # disable headless mode
+    options.add_argument(f"user-agent=Mozilla/5.0")
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    # Web driver (Chrome)
+    s = Service(r'chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+
+    # Upload page
+    driver.get(url)
+
+    # Scroll to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Take screenshots
+    driver.get_screenshot_as_png()
+    driver.save_screenshot(f"screenshots/{url.split('https://vk.com/')[-1]}.png")
+
+    # Close browser
+    driver.quit()
+
